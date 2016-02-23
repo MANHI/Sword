d8e2da4b06f5902b918dda780d18c9eca9ad7d06
MANHI <1751433686@qq.com>
2016-02-23 10:55:36 +0800

    41

---
+++ Pro_41.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void PrintSequence(int small, int big);
+
+void FindContinuousSeuence(int sum)
+{
+	int small = 1;
+	int big = 2;
+	int end = (sum + 1) / 2;
+	int cur = big + small;
+	while (small < end)
+	{
+		if (cur < sum)
+		{
+			big++;
+			cur += big;
+		}
+		else if (cur > sum)
+		{
+			cur -= small;
+			small++;
+		}
+		else
+		{
+			PrintSequence(small, big);
+			cur -= small;
+			small++;
+		}
+	}
+}
+
+void PrintSequence(int small, int big)
+{
+	cout << small << "-" << big << endl;
+}
+
+int main(void)
+{
+	int sum;
+	cin >> sum;
+	FindContinuousSeuence(sum);
+	return 0;
+}
